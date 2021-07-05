8c31c2373c978e0775bca4822722cf03c6bef8ca
David Roy <droy@worldwideservices.net>
2021-07-05 12:57:42 -0500

    prelim push - html-1 not moving

---
+++ index.js
@@ -0,0 +1,30 @@
+function modulo(number, mod) {
+let result = number % mod;
+if (result <0) {
+    result *=
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+// let position = 0;
+
+// const carousel-button_next = function ()  {
+//  const imageSlider = document.getElementById('imageSlider')
+//  position -= 400;
+//  imageSlider.style.transform = `translateX(${position}px)`;
+// }
+
+// const prevSlide = function ()  {
+//     const imageSlider = document.getElementById('imageSlider')
+//       position += 400;
+//      imageSlider.style.transform = `translateX(${position}px)`;
+// }
