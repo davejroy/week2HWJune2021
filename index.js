3b7f87b513d5b602edd9886ea4745f99e70ed20f
David Roy <droy@worldwideservices.net>
2021-07-07 20:55:30 -0500

    almost complete... a little more work

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-function modulo(number, mod) {
-let result = number % mod;
-if (result <0) {
-    result *=
+let position = 0;
+
+const nextSlide = function ()  {
+ const imageSlider = document.getElementById('imageSlider')
+    position -= 800;
+    imageSlider.style.transform = `translateX(${position}px)`;
+    console.log(position);
 }
-
-
+const prevSlide = function ()  {
+    const imageSlider = document.getElementById('imageSlider')
+     position += 800;
+     imageSlider.style.transform = `translateX(${position}px)`;
 }
-
-
-
-
-
-
-
-
-
-// let position = 0;
-
-// const carousel-button_next = function ()  {
-//  const imageSlider = document.getElementById('imageSlider')
-//  position -= 400;
-//  imageSlider.style.transform = `translateX(${position}px)`;
-// }
-
-// const prevSlide = function ()  {
-//     const imageSlider = document.getElementById('imageSlider')
-//       position += 400;
-//      imageSlider.style.transform = `translateX(${position}px)`;
-// }
